04806cf8b6fb41e38a6e7cfe54ef21bd0355b5b4
Roman Dibikhin <dibikhin@gmail.com>
2016-04-21 02:04:42 +0300

    todos & info moved to readme

--- README.md
+++ README.md
@@ -9,3 +9,20 @@
 `npm start`
 
 Should print a log of actions without errors.
+
+## TODO
+- introduce mediator's factory DONE
+- extract fakes: customers, payments, stats DONE
+- all 'inits' should be run before first 'run'
+- use '.lib/*' path
+- subscribe to global 'error' event
+
+## Info
+
+- debugging events: console.log(mediator) will show events and options
+- navigate through events with global text search
+- possible bugs:
+    - forgot to bootstrap
+    - forgot to subscribe
+    - module string names
+    - events' names mismatch
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mediator = require('./mediator_factory').create();
 const bootstrap = require('./bootstrap');
 
 const app = require('./app');
+// const modules = require('./modules');
 
 console.log('Server starting...');
 console.log('Bootstrapping...');
@@ -22,19 +23,3 @@ console.log('Server started.');
 console.log('App running...');
 
 app.run(mediator);
-
-// TODO introduce mediator's factory <<DONE>>
-// TODO extract fakes: customers, payments, stats <<DONE>>
-// TODO all 'inits' should be run before first 'run'
-// TODO use '.lib/*' path
-// TODO subs. on global 'error' event
-
-// INFO debugging events: console.log(mediator) will show events and options
-
-// INFO navigate through events with global text search
-
-// INFO possible bugs:
-// - forgot to bootstrap
-// - forgot to subscribe
-// - module string names
-// - events' names mismatch
